379345bd1695b96a309cab5f67a0cbbb5a083863
alfon <alfon@DESKTOP-A03724H>
2020-04-19 12:21:35 +0200

    Creacion de entidades Causa y donacion

---
+++ src/main/java/org/springframework/samples/petclinic/model/Donacion.java
@@ -0,0 +1,28 @@
+
+package org.springframework.samples.petclinic.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "donacion")
+public class Donacion extends BaseEntity {
+
+	@Column(name = "cantidad")
+	@Min(value = 0)
+	@NotNull
+	private Integer	cantidad;
+
+	@ManyToOne
+	@JoinColumn(name = "causa_id")
+	private Causa	causa;
+
+}
